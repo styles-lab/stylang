0655ae4082153e4b6dc7bfa9c09b3e964e000057
yayanyang <yayanyang@gmail.com>
2025-04-04 15:38:15 +0800

    update

--- src/lang/block.rs
+++ src/lang/block.rs
@@ -1,8 +1,39 @@
-use super::Delimiter;
+use parserc::Parse;
+
+use super::{Delimiter, ParseError, StylangInput};
+
+/// A variable declaration stmt, be like: `let .. = ...;`
+#[derive(Debug, PartialEq, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Let<I> {
+    /// let stmt keyword `let`.
+    pub keyword: I,
+}
+
+/// Variant for in block stmt.
+#[derive(Debug, PartialEq, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum BlockStmt<I> {
+    Let(Let<I>),
+}
 
 /// A code block delimited by `{...}`.
-#[derive(Debug, PartialEq, PartialOrd, Clone)]
+#[derive(Debug, PartialEq, Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Block<I> {
+    /// delimiter of the block: `{...}`
     pub delimiter: Delimiter<I>,
+    /// stmts belongs to this block.
+    pub stmts: Vec<BlockStmt<I>>,
+}
+
+impl<I> Parse<I> for Block<I>
+where
+    I: StylangInput,
+{
+    type Error = ParseError;
+
+    fn parse(_input: I) -> parserc::Result<Self, I, Self::Error> {
+        todo!()
+    }
 }
