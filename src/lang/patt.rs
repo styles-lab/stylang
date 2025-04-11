a20c93eba5e2066cebdc4d3ff726ccfc8645d039
yayanyang <yayanyang@gmail.com>
2025-04-11 19:26:46 +0800

    udpate

--- src/lang/patt.rs
+++ src/lang/patt.rs
@@ -1,4 +1,8 @@
-use super::{AttrOrComment, Ident, Lit, Type};
+use parserc::Parse;
+
+use crate::lang::parse_attr_comment_list;
+
+use super::{AttrOrComment, Ident, Lit, ParseError, StylangInput, Type};
 
 /// A pattern in a local binding, function signature, match expression, or various other places.
 #[derive(Debug, PartialEq, Clone)]
@@ -31,4 +35,59 @@ pub struct PattIdent<I> {
     pub attr_comment_list: Vec<AttrOrComment<I>>,
     /// variable ident.
     pub ident: Ident<I>,
+}
+
+impl<I> Parse<I> for PattIdent<I>
+where
+    I: StylangInput,
+{
+    type Error = ParseError;
+
+    fn parse(input: I) -> parserc::Result<Self, I, Self::Error> {
+        let (attr_comment_list, input) = parse_attr_comment_list(input)?;
+        let (ident, input) = Ident::parse(input)?;
+
+        Ok((
+            Self {
+                attr_comment_list,
+                ident,
+            },
+            input,
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use parserc::Parse;
+
+    use crate::lang::{Attr, AttrOrComment, Comment, Ident, TokenStream};
+
+    use super::PattIdent;
+
+    #[test]
+    fn test_patt_ident() {
+        assert_eq!(
+            PattIdent::parse(TokenStream::from(
+                r#"
+                /// hello
+                @hello   ident
+                "#
+            )),
+            Ok((
+                PattIdent {
+                    attr_comment_list: vec![
+                        AttrOrComment::Comment(Comment(TokenStream::from((20, " hello")))),
+                        AttrOrComment::Attr(Attr {
+                            keyword: TokenStream::from((43, "@")),
+                            ident: Ident(TokenStream::from((44, "hello"))),
+                            body: None
+                        })
+                    ],
+                    ident: Ident(TokenStream::from((52, "ident"))),
+                },
+                TokenStream::from((57, "\n                "))
+            ))
+        );
+    }
 }
